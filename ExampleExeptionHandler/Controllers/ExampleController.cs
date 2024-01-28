4002e00830a455bff61ae2ab7e8ffe7adb9452c3
Anthony Finger <prostorua@outlook.com>
2024-01-28 15:53:02 +0200

    Add project files.

---
+++ ExampleExeptionHandler/Controllers/ExampleController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleExeptionHandler.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExampleController : ControllerBase
+    {
+        [HttpGet("GetAll")]
+        public Task<IActionResult> GetAllAsync()
+        {
+            throw new Exception();
+        }
+    }
+
+
+}
---
+++ ExampleExeptionHandler/Extention/GlobalExeption.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace ExampleExeptionHandler.Extention
+{
+    public class GlobalExeption : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            var problem = new NewResponce
+            {
+                StatusCode = (int)HttpStatusCode.InternalServerError,
+                Message = exception.Message,
+                Typy = exception.GetType().Name
+            };
+            await httpContext.Response.WriteAsJsonAsync(problem);
+            return true;
+        }
+    }
+
+    public class NewResponce
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public string Typy { get; set; }
+    }
+}
